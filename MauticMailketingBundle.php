ea0d774186117626c6637558eefa54592d652ab5
pekanbarugamer <pekanbarugamer@users.noreply.github.com>
2020-04-30 03:05:36 +0700

    Fix

--- MauticMailketingBundle.php
+++ MauticMailketingBundle.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace MauticPlugin\MauticMailketingBundle;
-
+use MauticPlugin\MauticMailketingBundle\DependencyInjection\Compiler\EmailTransportPass;
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MauticMailketingBundle extends PluginBundleBase
 {
